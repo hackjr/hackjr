fadcb20d025666e59d30238ca2b22f5dc916e89d
bfiegel <bfiegel@engineerweb.org>
2013-11-23 16:58:13 -0600

    processing Serial I/O

--- sensor/controller/controller.ino
+++ sensor/controller/controller.ino
@@ -1,18 +1,10 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  ReadAnalogVoltage
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
- 
-  This example code is in the public domain.
-*/
+#define MAX(a,b) ((a>b)?a:b) 
+#define MIN(a,b) ((a<b)?a:b)
  
 // Digital Output
-int RGB_RED   = 13; //Red PWM
-int RGB_GREEN = 11; //Green PWM
-int RGB_BLUE  = 10; //Blue PWM
+int RGB0_RED   = 13; //Red PWM
+int RGB0_GREEN = 11; //Green PWM
+int RGB0_BLUE  = 10; //Blue PWM
 
 int LED0      = 9; //Green PWM
 int LED1      = 12;
@@ -20,7 +12,7 @@ int LED2      = 1;
 int LED3      = 6; // PWM
 int LED4      = 5; // PWM
 
-int BUZZER = 0; // PWM
+int BUZZER0 = 0; // PWM
 
 // Digital Input
 int BUTTON0   = 3; //Interrupt capable
@@ -32,11 +24,11 @@ int SWITCH0   = 7;
 int SWITCH1   = 8;
 
 /* Analog In */
-int PHOTO_RESISTOR = A5;
-int PONTENTIOMETER = A0;
-int ACCEL_X        = A1;
-int ACCEL_Y        = A2;
-int ACCEL_Z        = A3;
+int PHOTO_RESISTOR0 = A5;
+int PONTENTIOMETER0 = A0;
+int ACCEL0_X        = A1;
+int ACCEL0_Y        = A2;
+int ACCEL0_Z        = A3;
 
 String oper            = "";
 String resource        = "";
@@ -44,28 +36,30 @@ String parameters      = "";
 String inputString     = "";         // a string to hold incoming data
 volatile boolean stringComplete = false;  // whether the string is complete
 
-boolean reportAccel  = false;
-boolean reportButton = true;
-boolean reportLight  = true;
-boolean reportSwitch = true;
-boolean reportTemp   = false;
+volatile boolean reportAccel  = false;
+volatile boolean reportButton = true;
+volatile boolean reportLight  = true;
+volatile boolean reportSwitch = true;
+volatile boolean reportTemp   = false;
 
-boolean button2Value = false;
-boolean button3Value = false;
+volatile boolean button0Value = false;
+volatile boolean button1Value = false;
+volatile boolean button2Value = false;
+volatile boolean button3Value = false;
 
-int     light0Value;
-int     switch0Value;
-int     switch1Value;
+volatile int     light0Value;
+volatile int     switch0Value;
+volatile int     switch1Value;
 
-int     accel_x;
-int     accel_y;
-int     accel_z;
+volatile int     accel_x;
+volatile int     accel_y;
+volatile int     accel_z;
 
 void button0_interrupt(void)
 {
   int state;
   state = digitalRead(BUTTON0);
-  Serial.print("/app/button/0 ");
+  Serial.print("/app/button/0/v ");
   if (LOW == state)
   {
     Serial.println(1);
@@ -80,7 +74,7 @@ void button1_interrupt(void)
 {
   int state;
   state = digitalRead(BUTTON1);
-  Serial.print("/app/button/1 ");
+  Serial.print("/app/button/1/v ");
   if (LOW == state)
   {
     Serial.println(1);
@@ -95,13 +89,13 @@ void printAccel(boolean force, char *tmp)
 {
   int x,y,z;
  
-  x = (analogRead(ACCEL_X)/4)-128;
-  y = (analogRead(ACCEL_Y)/4)-128;
-  z = (analogRead(ACCEL_Z)/4)-128;
+  x = (analogRead(ACCEL0_X)/4)-128;
+  y = (analogRead(ACCEL0_Y)/4)-128;
+  z = (analogRead(ACCEL0_Z)/4)-128;
   if ((true == force)
-      || (((accel_x - x) < -8) || ((accel_x -x) > 8))
-      || (((accel_y - y) < -8) || ((accel_y -y) > 8))
-      || (((accel_z - z) < -8) || ((accel_z -z) > 8))
+      || ((MAX(accel_x,x) - MIN(accel_x,x)) > 8)
+      || ((MAX(accel_y,y) - MIN(accel_y,y)) > 8)
+      || ((MAX(accel_z,z) - MIN(accel_z,z)) > 8)
      )
   {
     Serial.print(tmp);
@@ -114,14 +108,14 @@ void printAccel(boolean force, char *tmp)
     Serial.println("");
     accel_x = x;
     accel_y = y;
-    accel_x = z;
+    accel_z = z;
   }
 }
 
 void printLight(boolean force, char *tmp)
 {
   int v;
-  v = (analogRead(PHOTO_RESISTOR)/4)-128;
+  v = (analogRead(PHOTO_RESISTOR0)/4);
   if ((true == force)
       || ((light0Value-v)<-4) 
       || ((light0Value-v)> 4)
@@ -134,13 +128,30 @@ void printLight(boolean force, char *tmp)
 }
 
 
+void printTemp(boolean force, char *tmp)
+{
+#if 0
+  int v;
+  v = (analogRead(TEMP0)/4)-128;
+  if ((true == force)
+      || ((light0Value-v)<-4) 
+      || ((light0Value-v)> 4)
+     )
+  {
+     Serial.print("/app/temp/0/v ");
+     Serial.println(v);
+     temp0Value = v;
+  }
+#endif
+}
+
 // the setup routine runs once when you press reset:
 void setup()
 {              
   // Initialize the digital output pins
-  pinMode(RGB_RED, OUTPUT);
-  pinMode(RGB_GREEN, OUTPUT);
-  pinMode(RGB_BLUE, OUTPUT);
+  pinMode(RGB0_RED, OUTPUT);
+  pinMode(RGB0_GREEN, OUTPUT);
+  pinMode(RGB0_BLUE, OUTPUT);
   pinMode(LED0, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -156,9 +167,9 @@ void setup()
   pinMode(SWITCH1, INPUT_PULLUP);
   
   // Initialize the digital output pins as low
-  digitalWrite(RGB_RED, LOW);    
-  digitalWrite(RGB_GREEN, LOW);    
-  digitalWrite(RGB_BLUE, LOW);
+  analogWrite(RGB0_RED, 255);    
+  analogWrite(RGB0_GREEN, 255);    
+  analogWrite(RGB0_BLUE, 255);
   digitalWrite(LED0, LOW);
   digitalWrite(LED1, LOW);
   digitalWrite(LED2, LOW);
@@ -166,7 +177,8 @@ void setup()
   digitalWrite(LED4, LOW);
   
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(115200);
+  Serial.begin(9600);
+  
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
   resource.reserve(32);
@@ -177,17 +189,22 @@ void setup()
   parameters      = "";
   inputString     = ""; 
   
-  attachInterrupt(0, button0_interrupt, CHANGE);
-  attachInterrupt(1, button1_interrupt, CHANGE);
+  button0Value = digitalRead(BUTTON2);
+  button1Value = digitalRead(BUTTON3);
   button2Value = digitalRead(BUTTON2);
   button3Value = digitalRead(BUTTON3);
+  
   switch0Value = digitalRead(SWITCH0);
   switch1Value = digitalRead(SWITCH1);
-  light0Value  = (analogRead(PHOTO_RESISTOR)/4)-128;
+
+  light0Value  = (analogRead(PHOTO_RESISTOR0)/4);
   
-  accel_x = (analogRead(ACCEL_X)/4)-128;
-  accel_y = (analogRead(ACCEL_Y)/4)-128;
-  accel_z = (analogRead(ACCEL_Z)/4)-128;
+  accel_x = (analogRead(ACCEL0_X)/4)-128;
+  accel_y = (analogRead(ACCEL0_Y)/4)-128;
+  accel_z = (analogRead(ACCEL0_Z)/4)-128;
+
+//  attachInterrupt(0, button0_interrupt, CHANGE);
+//  attachInterrupt(1, button1_interrupt, CHANGE);
 }
 
 void handleAccel(int id)
@@ -221,7 +238,7 @@ void handleAccel(int id)
     }
     else
     {
-      Serial.println("Invalid Operator");
+      Serial.println("Invalid Resource");
       return;
     } 
   }
@@ -235,17 +252,42 @@ void handleAccel(int id)
 void handleButton(int id)
 {
   char tmp[32];
-  if ((4<=id) || (0>id))
+  int button;
+      
+  switch(id)
   {
-    Serial.println("Invalid Resource ID");
-    return;
+    case 0:
+    {
+      button=BUTTON0;
+      break;
+    } 
+    case 1:
+    {
+      button=BUTTON1;
+      break;
+    } 
+    case 2:
+    {
+      button=BUTTON2;
+      break;
+    } 
+    case 3:
+    {
+      button=BUTTON3;
+      break;
+    }
+    default:
+    {
+      Serial.println("Invalid Resource ID");
+      return;
+    }
   }
   if (oper.equals("SET"))
   {
     sprintf(tmp,"/app/button/%d/r",id);
     if (resource.equals(tmp))
     {
-      reportAccel = parameters.toInt();
+      reportButton = parameters.toInt();
       Serial.println("OK");
     }
     else
@@ -260,30 +302,7 @@ void handleButton(int id)
     if (resource.equals(tmp))
     {
       int value;
-      int button;
-      switch(id)
-      {
-        case 0:
-        {
-          button=BUTTON0;
-          break;
-        } 
-        case 1:
-        {
-          button=BUTTON1;
-          break;
-        } 
-        case 2:
-        {
-          button=BUTTON2;
-          break;
-        } 
-        case 3:
-        {
-          button=BUTTON3;
-          break;
-        }
-      }
+
       value=digitalRead(button);
       Serial.print(tmp);
       Serial.print(" ");
@@ -292,7 +311,7 @@ void handleButton(int id)
     }
     else
     {
-      Serial.println("Invalid Operator");
+      Serial.println("Invalid Resource");
       return;
     } 
   }
@@ -316,7 +335,7 @@ void handleBuzzer(int id)
     sprintf(tmp,"/app/buzzer/%d/v",id);
     if (resource.equals(tmp))
     {
-      analogWrite(BUZZER0, parameters.toInt());
+      tone(BUZZER0, parameters.toInt());
       Serial.println("OK");
     }
     else
@@ -330,7 +349,7 @@ void handleBuzzer(int id)
     sprintf(tmp,"/app/buzzer/%d/v",id);
     if (resource.equals(tmp))
     {
-      int value
+      int value;
       value=analogRead(BUZZER0);
       Serial.print(tmp);
       Serial.print(" ");
@@ -339,7 +358,7 @@ void handleBuzzer(int id)
     }
     else
     {
-      Serial.println("Invalid Operator");
+      Serial.println("Invalid Resource");
       return;
     } 
   }
@@ -353,17 +372,48 @@ void handleBuzzer(int id)
 void handleLed(int id)
 {
   char tmp[32];
-  if (0!=id)
+  int led;
+  
+  switch(id)
   {
-    Serial.println("Invalid Resource ID");
-    return;
-  }
+    case 0:
+    {
+      led=LED0;
+      break;
+    } 
+    case 1:
+    {
+      led=LED1;
+      break;
+    } 
+    case 2:
+    {
+      led=LED2;
+      break;
+    } 
+    case 3:
+    {
+      led=LED3;
+      break;
+    } 
+    case 4:
+    {
+      led=LED4;
+      break;
+    }
+    default:
+    {
+      Serial.println("Invalid Resource ID");
+      return;
+    }
+  }  
+  
   if (oper.equals("SET"))
   {
     sprintf(tmp,"/app/led/%d/v",id);
     if (resource.equals(tmp))
     {
-      analogWrite(BUZZER0, parameters.toInt());
+      digitalWrite(led, parameters.toInt());
       Serial.println("OK");
     }
     else
@@ -377,42 +427,16 @@ void handleLed(int id)
     sprintf(tmp,"/app/led/%d/v",id);
     if (resource.equals(tmp))
     {
-      int value
-      int led;
-      switch(id)
-      {
-        case 0:
-        {
-          led=LED0;
-          break;
-        } 
-        case 1:
-        {
-          led=LED1;
-          break;
-        } 
-        case 2:
-        {
-          led=LED2;
-          break;
-        } 
-        case 3:
-        {
-          led=LED3;
-          break;
-        } 
-        case 4:
-        {
-          led=LED4;
-          break;
-        }
-      }
-      value=digitalWrite(led,paramaters.toInt());
-      Serial.println("OK");
+      int value;
+      value = digitalRead(led);
+      Serial.print(tmp);
+      Serial.print(" ");
+      Serial.print(value);
+      Serial.println("");
     }
     else
     {
-      Serial.println("Invalid Operator");
+      Serial.println("Invalid Resource");
       return;
     } 
   }
@@ -422,7 +446,6 @@ void handleLed(int id)
     return;
   }
 }
-
 
 void handleLight(int id)
 {
@@ -455,7 +478,7 @@ void handleLight(int id)
     }
     else
     {
-      Serial.println("Invalid Operator");
+      Serial.println("Invalid Resource");
       return;
     } 
   }
@@ -466,12 +489,226 @@ void handleLight(int id)
   }
 }
 
+void handleRgb(int id)
+{
+  char tmp[32];
+  if (0!=id)
+  {
+    Serial.println("Invalid Resource ID");
+    return;
+  }
+  if (oper.equals("SET"))
+  {
+    sprintf(tmp,"/app/rgb/%d/v",id);
+    if (resource.equals(tmp))
+    {
+      int greenIndex,blueIndex;
+      greenIndex = parameters.indexOf(" ",1);
+      blueIndex = parameters.indexOf(" ",1+greenIndex);
+      analogWrite(255 - RGB0_RED, parameters.toInt());
+      analogWrite(255- RGB0_GREEN, parameters.substring(greenIndex).toInt());
+      analogWrite(255- RGB0_BLUE, parameters.substring(blueIndex).toInt());
+      Serial.println("OK");
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("GET"))
+  {
+    sprintf(tmp,"/app/rgb/%d/v",id);
+    if (resource.equals(tmp))
+    {
+      int value;
+
+      Serial.print(tmp);
+      Serial.print(" ");
+      value=analogRead(RGB0_RED);
+      Serial.print(255-value);
+      value=analogRead(RGB0_GREEN);
+      Serial.print(255-value);
+      value=analogRead(RGB0_BLUE);
+      Serial.print(255-value);
+      Serial.println("");
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("EXECUTE"))
+  {
+    Serial.println("Invalid Operator");
+    return;
+  }
+}
+
+#if 0
+void handleServo(int id)
+{
+  char tmp[32];
+  if (0!=id)
+  {
+    Serial.println("Invalid Resource ID");
+    return;
+  }
+  if (oper.equals("SET"))
+  {
+    sprintf(tmp,"/app/servo/%d/v",id);
+    if (resource.equals(tmp))
+    {
+      tone(SERVO0, parameters.toInt());
+      Serial.println("OK");
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("GET"))
+  {
+    sprintf(tmp,"/app/servo/%d/v",id);
+    if (resource.equals(tmp))
+    {
+      int value;
+      value=analogRead(SERVO0);
+      Serial.print(tmp);
+      Serial.print(" ");
+      Serial.print(value);
+      Serial.println("");
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("EXECUTE"))
+  {
+    Serial.println("Invalid Operator");
+    return;
+  }
+}
+#endif
+
+void handleSwitch(int id)
+{
+  char tmp[32];
+  int s;
+  
+  switch(id)
+  {
+    case 0:
+    {
+      s=SWITCH0;
+      break;
+    } 
+    case 1:
+    {
+      s=SWITCH1;
+      break;
+    }
+    default:
+    {
+      Serial.println("Invalid Resource ID");
+      return;
+    }
+  }
+  if (oper.equals("SET"))
+  {
+    sprintf(tmp,"/app/switch/%d/r",id);
+    if (resource.equals(tmp))
+    {
+      reportSwitch = parameters.toInt();
+      Serial.println("OK");
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("GET"))
+  {
+    sprintf(tmp,"/app/switch/%d/v",id);
+    if (resource.equals(tmp))
+    {
+      int value;
+
+      value=digitalRead(s);
+      Serial.print(tmp);
+      Serial.print(" ");
+      Serial.print(value);
+      Serial.println("");
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("EXECUTE"))
+  {
+    Serial.println("Invalid Operator");
+    return;
+  }
+}
+
+#if 0
+void handleTemp(int id)
+{
+  char tmp[32];
+  if (0 != id)
+  {
+    Serial.println("Invalid Resource ID");
+    return;
+  }
+  if (oper.equals("SET"))
+  {
+    sprintf(tmp,"/app/temp/%d/r",id);
+    if (resource.equals(tmp))
+    {
+      reportTemp = parameters.toInt();
+      Serial.println("OK");
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("GET"))
+  {
+    sprintf(tmp,"/app/temp/%d/v",id);
+    if (resource.equals(tmp))
+    {
+      printTemp(true, tmp);
+    }
+    else
+    {
+      Serial.println("Invalid Resource");
+      return;
+    } 
+  }
+  else if (oper.equals("EXECUTE"))
+  {
+    Serial.println("Invalid Operator");
+    return;
+  }
+}
+#endif
 
 void handleApp(void)
 {
-  int id;
-  id  = resource[5] - '0';
-  if ('/' != resource[6])
+  int id,idIndex;
+  
+  idIndex = resource.indexOf("/",5)+1;
+  id  = resource[idIndex] - '0';
+  if ('/' != resource[idIndex+1])
   {
     Serial.println("Bad Resource");
     return;
@@ -506,15 +743,58 @@ void handleApp(void)
       handleLight(id);
     }
     else if (resource.startsWith("/app/rgb/"))
-    { 
+    {
+      handleRgb(id); 
     }
+#if 0
     else if (resource.startsWith("/app/servo/"))
-    { 
+    {
+      handleServo(id); 
     }
+#endif
     else if (resource.startsWith("/app/switch/"))
+    {
+      handleSwitch(id);
+    }
+#if 0
+    else if (resource.startsWith("/app/temp/"))
     { 
     }
-    else if (resource.startsWith("/app/temp/"))
+#endif
+    else
+    {
+      Serial.println("Bad Resource");
+    }
+  }
+}
+
+void handleArduino(void)
+{
+  int id,idIndex;
+  
+  idIndex = resource.indexOf("/",5)+1;
+  id  = resource[idIndex] - '0';
+  if ('/' != resource[idIndex+1])
+  {
+    Serial.println("Bad Resource");
+    return;
+  }
+  else
+  {
+
+    if (oper.equals("EXECUTE"))
+    {
+      Serial.println("Invalid Operator for given resource");
+      return; 
+    }
+      
+    if (resource.startsWith("/arduino/a/"))
+    { 
+    }
+    else if (resource.startsWith("/arduino/d/"))
+    {
+    }
+    else if (resource.startsWith("/arduino/pwm/"))
     { 
     }
     else
@@ -527,6 +807,10 @@ void handleApp(void)
 // the loop routine runs over and over again forever:
 void loop() 
 {
+#ifndef serialEvent_implemented
+  if (Serial.available()) serialEvent();
+#endif
+
   if(reportAccel)
   {
     printAccel(false, "/app/accel/0/v");
@@ -535,9 +819,26 @@ void loop()
   if(reportButton)
   {
      int tmp;
+     tmp = digitalRead(BUTTON0);
+     if (tmp != button0Value)
+     {
+       Serial.println(inputString);
+       Serial.print("/app/button/0/v ");
+       Serial.println(tmp);
+       button0Value = tmp;
+     }
+     tmp = digitalRead(BUTTON1);
+     if (tmp != button1Value)
+     {
+       Serial.println(inputString);
+       Serial.print("/app/button/1/v ");
+       Serial.println(tmp);
+       button1Value = tmp;
+     }
      tmp = digitalRead(BUTTON2);
      if (tmp != button2Value)
      {
+       Serial.println(inputString);
        Serial.print("/app/button/2/v ");
        Serial.println(tmp);
        button2Value = tmp;
@@ -574,6 +875,7 @@ void loop()
   }
   if (reportTemp)
   {
+    printTemp(false, "/app/temp/0/v");
   }
   // print the string when a newline arrives:
   if (stringComplete)
@@ -584,18 +886,7 @@ void loop()
     }
     else if (resource.startsWith("/arduino/"))
     {
-      if (resource.startsWith("a/"))
-      { 
-      }
-      else if (resource.startsWith("d/"))
-      { 
-      }
-      else if (resource.startsWith("pwm/"))
-      { 
-      }
-      else
-      {
-      }
+      handleArduino();
     }
     // clear the string:
     inputString = "";
@@ -610,9 +901,12 @@ void loop()
  response.  Multiple bytes of data may be available.
  */
 void serialEvent() {
+  
+  digitalWrite(LED1, 1);
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
+
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
@@ -627,7 +921,6 @@ void serialEvent() {
       {
         oper          = "GET";
         resourceIndex = 4;
-        
       }
       else if (inputString.startsWith("EXECUTE "))
       {
